a37b86789fb5dfa67e94cccb82570c6969e3c735
truongmanhdung <truongmanhdung04@gmail.com>
2025-07-18 00:06:15 +0700

    fix bug

--- app.js
+++ app.js
@@ -1,44 +1,45 @@
-import express from 'express';
-import { ApolloServer} from 'apollo-server-express';
-import cors from 'cors';
-import morgan from 'morgan'
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import cors from "cors";
+import morgan from "morgan";
 // load schema & resolvers
-import 'reflect-metadata'
-import typeDefs from './schema/schema';
-import resolvers from './resolver/resolver';
+import "reflect-metadata";
+import typeDefs from "./schema/schema.js";
+import resolvers from "./resolver/resolver.js";
 
 // load db methods
 
-import mongooseDataMethods from './data/db';
+import mongooseDataMethods from "./data/db";
 
 // connect mongose db
-require('dotenv').config()
+require("dotenv").config();
 
-const db = require('./connectDb')
-db.connect()
+const db = require("./connectDb");
+db.connect();
 const server = new ApolloServer({
-    typeDefs,
-    resolvers
-})
+	typeDefs,
+	resolvers,
+});
 const app = express();
-app.use(cors())
-app.use(express.json())
-app.use(morgan('tiny'))
+app.use(cors());
+app.use(express.json());
+app.use(morgan("tiny"));
 
 async function startServer() {
-    const apolloServer = new ApolloServer({
-        typeDefs,
-        resolvers,
-        context: () => ({mongooseDataMethods})
-    });
-    await apolloServer.start();
-    apolloServer.applyMiddleware({ app });
+	const apolloServer = new ApolloServer({
+		typeDefs,
+		resolvers,
+		context: () => ({ mongooseDataMethods }),
+	});
+	await apolloServer.start();
+	apolloServer.applyMiddleware({ app });
 }
 startServer();
 
-
 const port = 9000;
 
-app.listen({port}, () => {
-    console.log(`Server listening on http://localhost:${port}${server.graphqlPath}`)
-})
+app.listen({ port }, () => {
+	console.log(
+		`Server listening on http://localhost:${port}${server.graphqlPath}`,
+	);
+});
